50e434dd8f2454ae3ce7ec322b21ed6613ffd232
Edwin Cruz <edwincruz25@unitec.edu>
2017-12-16 20:28:56 -0600

    react deplyment

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    con: 'Edwin',
+  };
+
+  componentDidMount() {
+    fetch('/asd')
+      .then(res => res.json())
+      .then(data => this.setState({ con: data }));
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +20,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.con}</p>
       </div>
     );
   }
